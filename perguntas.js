554a6cfd714cc9387a1782ed875b5e2ccf93742b
Bibia3b <00001100903926SP@al.educacao.sp.gov.br>
2024-11-13 08:56:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Literatura',
+    'Quem escreveu o livro O Príncipe?',
+    'Maquíavel'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 1+1?',
+    'A resposta é 2'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
